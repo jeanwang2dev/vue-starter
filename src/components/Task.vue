6e9c4b8d60c9d5f1675c393fa91fe3a701050c5d
jeanwang2dev <jeanharmsen@yahoo.com>
2021-10-25 19:58:49 -0400

    add emit event for delete task

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,7 @@
 <template>
     <div :class="[task.reminder ? 'reminder' : '' , 'task']">
         <h3>{{ task.text }}
-            <i class="fas fa-times"></i>
+            <i @click="onDelete(task.id)" class="fas fa-times"></i>
         </h3>
         <p>{{ task.day }}</p>
     </div>
@@ -13,6 +13,12 @@ export default {
     name: 'Task',
     props: {
         task: Object
+    },
+    methods: {
+        onDelete(id){
+           //console.log("deleting " + id);
+           this.$emit('delete-task', id);
+        }
     }
 }
 </script>
